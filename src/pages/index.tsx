055e547cf8fd5f56e69f7835568616158d068638
Caelan <caelan.curry@gmail.com>
2022-10-07 18:39:49 +0200

    Update home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,23 @@
 import type { NextPage } from "next";
+import Head from "next/head";
+import Navbar from "../components/Navbar";
 import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
-  const campgroundsData = trpc.campground.getAll.useQuery();
+  // const campgroundsData = trpc.campground.getAll.useQuery();
 
-  const campgrounds = campgroundsData.data;
-  // console.log(campgrounds?.map((campground) => campground.title));
+  // const campgrounds = campgroundsData.data;
+  // // console.log(campgrounds?.map((campground) => campground.title));
 
   return (
-    <div>
-      <h1>Home</h1>
-      {campgrounds?.map((campground) => (
-        <p key={campground.id}>{campground.title}</p>
-      ))}
-    </div>
+    <>
+      <Head>
+        <title>Campfire - Next</title>
+      </Head>
+
+      <Navbar />
+    </>
   );
 };
 
 export default Home;
-
-// export async function getServerSideProps() {
-//   const campgrounds = await prisma.campground.findMany();
-//   return {
-//     props: { campgrounds }, // will be passed to the page component as props
-//   };
-// }
