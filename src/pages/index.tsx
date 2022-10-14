bfa8f20369aed05f947ef57e7a216a9b4a5d0809
Caelan <caelan.curry@gmail.com>
2022-10-14 18:43:31 +0200

    Added Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Navbar from "../components/Navbar";
-import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
   // const campgroundsData = trpc.campground.getAll.useQuery();
@@ -15,7 +13,27 @@ const Home: NextPage = () => {
         <title>Campfire - Next</title>
       </Head>
 
-      <Navbar />
+      <div
+        className="flex h-screen w-full bg-cover bg-center bg-no-repeat text-center text-white"
+        style={{
+          backgroundImage:
+            "url('https://res.cloudinary.com/dv5vm4sqh/image/upload/v1640878650/CampFire/campfire-cover_efr5mj.jpg')",
+        }}
+      >
+        <div className="max-w-60 align-center flex h-full w-full flex-col justify-center p-3">
+          <main className="px-3">
+            <h1 className="mb-3 text-5xl">Campfire</h1>
+            <p className="text-l">
+              Welcome to CampFire!
+              <br />
+              Find your dream getaway today.
+              <br />
+              Explore the nature of Earth and collaborate with others to find
+              unique spots.
+            </p>
+          </main>
+        </div>
+      </div>
     </>
   );
 };
